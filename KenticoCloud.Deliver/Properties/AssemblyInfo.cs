1885c5496d233b1a7bf6f4a570e25a32655f8dfe
MASTER\JiriS <jiris@kentico.com>
2016-10-25 11:06:18 +0200

    Updated nuget description and .net version

--- KenticoCloud.Deliver/Properties/AssemblyInfo.cs
+++ KenticoCloud.Deliver/Properties/AssemblyInfo.cs
@@ -2,14 +2,14 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("KenticoCloud.Deliver")]
-[assembly: AssemblyDescription("SDK for Kentico Deliver")]
+[assembly: AssemblyDescription("Kentico Deliver SDK for .NET 4.5")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Kentico Software")]
-[assembly: AssemblyProduct("Kentico Cloud Deliver .NET SDK")]
+[assembly: AssemblyProduct("Kentico Deliver SDK for .NET")]
 [assembly: AssemblyCopyright("© 2016 Kentico Software. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("103c7376-d90c-4207-9933-c9e55a16d902")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
