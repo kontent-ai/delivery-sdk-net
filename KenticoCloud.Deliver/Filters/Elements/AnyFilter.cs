ff1ec14981c2b2110cc316f8deb7f76b4dcf63e3
Jiří Kusák <huluvu21@seznam.cz>
2017-01-18 15:30:33 +0100

    Add new filters - any and all

---
+++ KenticoCloud.Deliver/Filters/Elements/AnyFilter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KenticoCloud.Deliver
+{
+    /// <summary>
+    /// Represents "any" operator.
+    /// </summary>
+    public class AnyFilter : AbstractFilter
+    {
+        /// <summary>
+        /// Constructs the Any filter.
+        /// </summary>
+        /// <param name="element">Element codename.</param>
+        /// <param name="value">Parameter value.</param>
+        public AnyFilter(string element, params string[] value)
+            : base(element, string.Join(",", value))
+        {
+            Operator = "[any]";
+        }
+    }
+}
