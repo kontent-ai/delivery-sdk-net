f03f4d0839886d5f8f01c79b53740036ff028ed2
Petr Švihlík <svihlik.petr@gmail.com>
2017-01-25 19:40:26 +0100

    usings cleanup (testing PR)

--- KenticoCloud.Deliver/Filters/Parameters/LimitFilter.cs
+++ KenticoCloud.Deliver/Filters/Parameters/LimitFilter.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace KenticoCloud.Deliver
+﻿namespace KenticoCloud.Deliver
 {
     /// <summary>
     /// Represents "limit" query parameter.
