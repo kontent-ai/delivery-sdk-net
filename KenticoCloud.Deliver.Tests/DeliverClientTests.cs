e572ec89708317db78c722fa4bd61783a76be501
Petr Švihlík <svihlik.petr@gmail.com>
2017-01-31 20:27:37 +0100

    fixed tests

--- KenticoCloud.Deliver.Tests/DeliverClientTests.cs
+++ KenticoCloud.Deliver.Tests/DeliverClientTests.cs
@@ -7,57 +7,63 @@ using NUnit.Framework;
 
 namespace KenticoCloud.Deliver.Tests
 {
-    [TestFixture]
-    public class DeliverClientTests
-    {
-        private const string PROJECT_ID = "ac9df07a-1251-4721-9ccf-6935ad5b6c1f";
+	[TestFixture]
+	public class DeliverClientTests
+	{
+		private const string PROJECT_ID = "e391c776-9d1e-4e1a-8a5a-1c327c2586b6";
 
-        [Test]
-        public void GetItemAsync()
-        {
-            var client = new DeliverClient(PROJECT_ID);
+		[Test]
+		public void GetItemAsync()
+		{
+			// Arrange
+			var client = new DeliverClient(PROJECT_ID);
 
-            var item = Task.Run(() => client.GetItemAsync("sdk_test_item")).Result.Item;
+			// Act
+			var item = Task.Run(() => client.GetItemAsync("coffee_beverages_explained")).Result.Item;
 
-            Assert.AreEqual("sdk_test_item", item.System.Codename);
+			var textElement = item.GetString("title");
+			var richTextElement = item.GetString("body_copy");
+			var datetimeElement = item.GetDateTime("post_date");
+			var assetElement = item.GetAssets("teaser_image");
+			var modularContentElement = item.GetModularContent("related_articles");
 
-            var textElement = item.GetString("text");
-            var richTextElement = item.GetString("rich_text");
-            var numberElement = item.GetNumber("number");
-            var datetimeElement = item.GetDateTime("datetime");
-            var assetElement = item.GetAssets("assets");
-            var modularContentElement = item.GetModularContent("modular_content");
-
-            Assert.AreEqual("random textz", textElement);
-            Assert.AreEqual("<p>this is <strong>rich</strong>!</p>", richTextElement);
-            Assert.AreEqual(21.63, numberElement);
-            Assert.AreEqual(DateTime.Parse("21.10.2016 0:00:00"), datetimeElement);
-            Assert.AreEqual(2, assetElement.Count);
-            Assert.AreEqual(4, modularContentElement.Count());
-        }
-
-
-        [Test]
-        public void GetItemAsync_NonExistentCodename()
-        {
-            var client = new DeliverClient(PROJECT_ID);
-
-            var response = Task.Run(() => client.GetItemAsync("sdk_test_item_non_existent")).Result;
-
-            Assert.IsNotNull(response);
-        }
+			// Assert
+			Assert.AreEqual("article", item.System.Type);
+			Assert.AreEqual("Coffee Beverages Explained", textElement);
+			Assert.That(() => richTextElement.Contains("Caffeine: &lt; 100 mg/cup<br/>"));
+			Assert.AreEqual(DateTime.Parse("18.11.2014 0:00:00"), datetimeElement);
+			Assert.AreEqual(1, assetElement.Count);
+			Assert.AreEqual(0, modularContentElement.Count());
+		}
 
 
-        [Test]
-        public void GetItemsAsync()
-        {
-            var client = new DeliverClient(PROJECT_ID);
+		[Test]
+		public void GetItemAsync_NonExistentCodename()
+		{
+			// Arrange
+			var client = new DeliverClient(PROJECT_ID);
 
-            var filters = new List<IFilter> { new EqualsFilter("system.type", "sdk_test_type") };
+			// Act
+			AsyncTestDelegate d = async () => await client.GetItemAsync("sdk_test_item_non_existent");
 
-            var response = Task.Run(() => client.GetItemsAsync(filters)).Result;
+			// Assert
+			Assert.ThrowsAsync<DeliverException>(d);
+		}
 
-            Assert.IsNotNull(response);
-        }
-    }
+
+		[Test]
+		public void GetItemsAsync()
+		{
+			// Arrange
+			var client = new DeliverClient(PROJECT_ID);
+			var filters = new List<IFilter> { new EqualsFilter("system.type", "cafe") };
+
+			// Act
+			var response = Task.Run(() => client.GetItemsAsync(filters)).Result;
+
+			// Assert
+			Assert.IsNotNull(response);
+			Assert.GreaterOrEqual(response.Items.Count, 1);
+		}
+	}
 }
