639f67fb7d3e41f198d8c7bdae58098af823f998
ondrejsevcik <ondrej.sev@gmail.com>
2017-02-23 11:11:22 +1100

    Fix GetStronglyTypedModularContent test case

--- KenticoCloud.Delivery.Tests/Models/Item/ContentItemTests.cs
+++ KenticoCloud.Delivery.Tests/Models/Item/ContentItemTests.cs
@@ -11,66 +11,17 @@ namespace KenticoCloud.Delivery.Tests
     [TestFixture]
     public class ContentItemTests
     {
-        // TODO: Unable to build due to change in visiblity level of ContentItem class from public to internal.
-        //[TestCase]
-        //public void CastTo_GetStronglyTypedModularContent()
-        //{
-//            var jsonDefinition = @"{
-//   ""item"":{
-//      ""system"":{
-//         ""id"":""cd17ddf8-7bc6-47de-8beb-0e15a2381d76"",
-//         ""name"":""Parent"",
-//         ""codename"":""parent"",
-//         ""type"":""article"",
-//         ""sitemap_locations"":[],
-//         ""last_modified"":""2017-02-22T00:52:03.9370993Z""
-//      },
-//      ""elements"":{
-//         ""title"":{
-//            ""type"":""text"",
-//            ""name"":""Title"",
-//            ""value"":""Title of Article""
-//         },
-//         ""related_articles"":{
-//            ""type"":""modular_content"",
-//            ""name"":""Modular content field"",
-//            ""value"":[
-//               ""article2""
-//            ]
-//         }
-//      }
-//   },
-//   ""modular_content"":{
-//      ""article2"":{
-//         ""system"":{
-//            ""id"":""b30d533f-9822-4518-8113-e4b3437641b5"",
-//            ""name"":""Article 2"",
-//            ""codename"":""article2"",
-//            ""type"":""article"",
-//            ""sitemap_locations"":[],
-//            ""last_modified"":""2017-02-21T04:29:42.9564205Z""
-//         },
-//         ""elements"":{
-//            ""title"":{
-//               ""type"":""text"",
-//               ""name"":""Title"",
-//               ""value"":""Title of Article 2""
-//            },
-//            ""related_articles"":{
-//                ""type"":""modular_content"",
-//                ""name"":""Modular content field"",
-//                ""value"":[]
-//            }
-//         }
-//      }
-//   }
-//}";
-//            JObject contentItem = JObject.Parse(jsonDefinition);
-//            ArticleModel parentArticle = new ContentItem(contentItem["item"], contentItem["modular_content"]).CastTo<ArticleModel>();
-//            ArticleModel relatedArticle = parentArticle.RelatedArticles.First().CastTo<ArticleModel>();
+        [TestCase]
+        public void CastTo_GetStronglyTypedModularContent()
+        {
+            const string SANDBOX_PROJECT_ID = "e1167a11-75af-4a08-ad84-0582b463b010";
+            var client = new DeliveryClient(SANDBOX_PROJECT_ID);
 
-//            Assert.AreEqual("Title of Article 2", relatedArticle.Title);
-//            Assert.AreEqual("Article 2", relatedArticle.System.Name);
-        //}
+            ArticleModel parentArticle = Task.Run(() => client.GetItemAsync<ArticleModel>("article_1")).Result.Item;
+            ArticleModel relatedArticle = parentArticle.RelatedArticles.First().CastTo<ArticleModel>();
+
+            Assert.AreEqual("Title of article 2", relatedArticle.Title);
+            Assert.AreEqual("Article 2", relatedArticle.System.Name);
+        }
     }
 }
