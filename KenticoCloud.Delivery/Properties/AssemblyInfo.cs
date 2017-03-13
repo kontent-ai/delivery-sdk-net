6ba9585d2e86dcd0a51dab2f6a869c5d319a9106
Ales Kalina <alesk@kentico.com>
2017-03-13 15:11:35 +0100

    Update product version

--- KenticoCloud.Delivery/Properties/AssemblyInfo.cs
+++ KenticoCloud.Delivery/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("© 2016 Kentico Software. All rights reserved.")]
 [assembly: ComVisible(false)]
 [assembly: Guid("103c7376-d90c-4207-9933-c9e55a16d902")]
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
 
 [assembly: InternalsVisibleTo("KenticoCloud.Delivery.Tests")]
