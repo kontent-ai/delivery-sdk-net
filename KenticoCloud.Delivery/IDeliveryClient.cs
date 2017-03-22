3dc41bdf9455f995ce6e3fcbdf807999c789cf48
Petr Švihlík <svihlik.petr@gmail.com>
2017-03-22 12:43:41 +0100

    extracted IDeliveryClient interface

---
+++ KenticoCloud.Delivery/IDeliveryClient.cs
@@ -0,0 +1,147 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace KenticoCloud.Delivery
+{
+    /// <summary>
+    /// Defines members necessary for retrieveing content and its metadata from the Kentico Cloud Delivery service.
+    /// </summary>
+    public interface IDeliveryClient
+    {
+        /// <summary>
+        /// Gets or sets an object that resolves links to content items in Rich text element values.
+        /// </summary>
+        IContentLinkUrlResolver ContentLinkUrlResolver { get; set; }
+
+        /// <summary>
+        /// Gets or sets an object that performs conversion of content items to code-first models.
+        /// </summary>
+        ICodeFirstModelProvider CodeFirstModelProvider { get; set; }
+
+        /// <summary>
+        /// Returns a content item as JSON data.
+        /// </summary>
+        /// <param name="codename">The codename of a content item.</param>
+        /// <param name="parameters">An array that contains zero or more query parameters, for example for projection or depth of modular content.</param>
+        /// <returns>The <see cref="JObject"/> instance that represents the content item with the specified codename.</returns>
+        Task<JObject> GetItemJsonAsync(string codename, params string[] parameters);
+
+        /// <summary>
+        /// Returns content items as JSON data.
+        /// </summary>
+        /// <param name="parameters">An array that contains zero or more query parameters, for example for filtering, ordering or depth of modular content.</param>
+        /// <returns>The <see cref="JObject"/> instance that represents the content items. If no query parameters are specified, all content items are returned.</returns>
+        Task<JObject> GetItemsJsonAsync(params string[] parameters);
+
+        /// <summary>
+        /// Returns a content item.
+        /// </summary>
+        /// <param name="codename">The codename of a content item.</param>
+        /// <param name="parameters">An array that contains zero or more query parameters, for example for projection or depth of modular content.</param>
+        /// <returns>The <see cref="DeliveryItemResponse"/> instance that contains the content item with the specified codename.</returns>
+        Task<DeliveryItemResponse> GetItemAsync(string codename, params IQueryParameter[] parameters);
+
+        /// <summary>
+        /// Gets one strongly typed content item by its codename.
+        /// </summary>
+        /// <typeparam name="T">Type of the code-first model. (Or <see cref="object"/> if the return type is not yet known.)</typeparam>
+        /// <param name="codename">The codename of a content item.</param>
+        /// <param name="parameters">An array that contains zero or more query parameters, for example for projection or depth of modular content.</param>
+        /// <returns>The <see cref="DeliveryItemResponse{T}"/> instance that contains the content item with the specified codename.</returns>
+        Task<DeliveryItemResponse<T>> GetItemAsync<T>(string codename, params IQueryParameter[] parameters);
+
+        /// <summary>
+        /// Returns a content item.
+        /// </summary>
+        /// <param name="codename">The codename of a content item.</param>
+        /// <param name="parameters">A collection of query parameters, for example for projection or depth of modular content.</param>
+        /// <returns>The <see cref="DeliveryItemResponse"/> instance that contains the content item with the specified codename.</returns>
+        Task<DeliveryItemResponse> GetItemAsync(string codename, IEnumerable<IQueryParameter> parameters);
+
+        /// <summary>
+        /// Gets one strongly typed content item by its codename.
+        /// </summary>
+        /// <typeparam name="T">Type of the code-first model. (Or <see cref="object"/> if the return type is not yet known.)</typeparam>
+        /// <param name="codename">The codename of a content item.</param>
+        /// <param name="parameters">A collection of query parameters, for example for projection or depth of modular content.</param>
+        /// <returns>The <see cref="DeliveryItemResponse{T}"/> instance that contains the content item with the specified codename.</returns>
+        Task<DeliveryItemResponse<T>> GetItemAsync<T>(string codename, IEnumerable<IQueryParameter> parameters = null);
+
+        /// <summary>
+        /// Searches the content repository for items that match the filter criteria.
+        /// Returns content items.
+        /// </summary>
+        /// <param name="parameters">An array that contains zero or more query parameters, for example for filtering, ordering or depth of modular content.</param>
+        /// <returns>The <see cref="DeliveryItemListingResponse"/> instance that contains the content items. If no query parameters are specified, all content items are returned.</returns>
+        Task<DeliveryItemListingResponse> GetItemsAsync(params IQueryParameter[] parameters);
+
+        /// <summary>
+        /// Returns content items.
+        /// </summary>
+        /// <param name="parameters">A collection of query parameters, for example for filtering, ordering or depth of modular content.</param>
+        /// <returns>The <see cref="DeliveryItemListingResponse"/> instance that contains the content items. If no query parameters are specified, all content items are returned.</returns>
+        Task<DeliveryItemListingResponse> GetItemsAsync(IEnumerable<IQueryParameter> parameters);
+
+        /// <summary>
+        /// Searches the content repository for items that match the filter criteria.
+        /// Returns strongly typed content items.
+        /// </summary>
+        /// <typeparam name="T">Type of the code-first model. (Or <see cref="object"/> if the return type is not yet known.)</typeparam>
+        /// <param name="parameters">An array that contains zero or more query parameters, for example for filtering, ordering or depth of modular content.</param>
+        /// <returns>The <see cref="DeliveryItemListingResponse{T}"/> instance that contains the content items. If no query parameters are specified, all content items are returned.</returns>
+        Task<DeliveryItemListingResponse<T>> GetItemsAsync<T>(params IQueryParameter[] parameters);
+
+        /// <summary>
+        /// Searches the content repository for items that match the filter criteria.
+        /// Returns strongly typed content items.
+        /// </summary>
+        /// <typeparam name="T">Type of the code-first model. (Or <see cref="object"/> if the return type is not yet known.)</typeparam>
+        /// <param name="parameters">A collection of query parameters, for example for filtering, ordering or depth of modular content.</param>
+        /// <returns>The <see cref="DeliveryItemListingResponse{T}"/> instance that contains the content items. If no query parameters are specified, all content items are returned.</returns>
+        Task<DeliveryItemListingResponse<T>> GetItemsAsync<T>(IEnumerable<IQueryParameter> parameters);
+
+        /// <summary>
+        /// Returns a content type as JSON data.
+        /// </summary>
+        /// <param name="codename">The codename of a content type.</param>
+        /// <returns>The <see cref="JObject"/> instance that represents the content type with the specified codename.</returns>
+        Task<JObject> GetTypeJsonAsync(string codename);
+
+        /// <summary>
+        /// Returns content types as JSON data.
+        /// </summary>
+        /// <param name="parameters">An array that contains zero or more query parameters, for example for paging.</param>
+        /// <returns>The <see cref="JObject"/> instance that represents the content types. If no query parameters are specified, all content types are returned.</returns>
+        Task<JObject> GetTypesJsonAsync(params string[] parameters);
+
+        /// <summary>
+        /// Returns a content type.
+        /// </summary>
+        /// <param name="codename">The codename of a content type.</param>
+        /// <returns>The content type with the specified codename.</returns>
+        Task<ContentType> GetTypeAsync(string codename);
+
+        /// <summary>
+        /// Returns content types.
+        /// </summary>
+        /// <param name="parameters">An array that contains zero or more query parameters, for example for paging.</param>
+        /// <returns>The <see cref="DeliveryTypeListingResponse"/> instance that represents the content types. If no query parameters are specified, all content types are returned.</returns>
+        Task<DeliveryTypeListingResponse> GetTypesAsync(params IQueryParameter[] parameters);
+
+        /// <summary>
+        /// Returns content types.
+        /// </summary>
+        /// <param name="parameters">A collection of query parameters, for example for paging.</param>
+        /// <returns>The <see cref="DeliveryTypeListingResponse"/> instance that represents the content types. If no query parameters are specified, all content types are returned.</returns>
+        Task<DeliveryTypeListingResponse> GetTypesAsync(IEnumerable<IQueryParameter> parameters);
+
+        /// <summary>
+        /// Returns a content element.
+        /// </summary>
+        /// <param name="contentTypeCodename">The codename of the content type.</param>
+        /// <param name="contentElementCodename">The codename of the content element.</param>
+        /// <returns>A content element with the specified codename that is a part of a content type with the specified codename.</returns>
+        Task<ContentElement> GetContentElementAsync(string contentTypeCodename, string contentElementCodename);
+    }
+}
