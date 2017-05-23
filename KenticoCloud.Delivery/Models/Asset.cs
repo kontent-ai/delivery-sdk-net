fae2c0a93bf729e6984d0ab33b3dc64a0790780a
Petr Švihlík <svihlik.petr@gmail.com>
2017-05-23 08:42:07 +0200

    fixed spaces

--- KenticoCloud.Delivery/Models/Asset.cs
+++ KenticoCloud.Delivery/Models/Asset.cs
@@ -2,51 +2,51 @@
 
 namespace KenticoCloud.Delivery
 {
-    /// <summary>
-    /// Represents a digital asset, such as a document or image.
-    /// </summary>
-    public sealed class Asset
-    {
-        /// <summary>
-        /// Gets the name of the asset.
-        /// </summary>
-        [JsonProperty("name")]
-        public string Name { get; }
+	/// <summary>
+	/// Represents a digital asset, such as a document or image.
+	/// </summary>
+	public sealed class Asset
+	{
+		/// <summary>
+		/// Gets the name of the asset.
+		/// </summary>
+		[JsonProperty("name")]
+		public string Name { get; }
 
 		/// <summary>
 		/// Gets the description of the asset.
 		/// </summary>
 		[JsonProperty("description")]
-	    public string Description { get; }
+		public string Description { get; }
 
-        /// <summary>
-        /// Gets the media type of the asset, for example "image/jpeg".
-        /// </summary>
-        [JsonProperty("type")]
-        public string Type { get; }
+		/// <summary>
+		/// Gets the media type of the asset, for example "image/jpeg".
+		/// </summary>
+		[JsonProperty("type")]
+		public string Type { get; }
 
-        /// <summary>
-        /// Gets the asset size in bytes.
-        /// </summary>
-        [JsonProperty("size")]
-        public int Size { get; }
+		/// <summary>
+		/// Gets the asset size in bytes.
+		/// </summary>
+		[JsonProperty("size")]
+		public int Size { get; }
 
-        /// <summary>
-        /// Gets the URL of the asset.
-        /// </summary>
-        [JsonProperty("url")]
-        public string Url { get; }
+		/// <summary>
+		/// Gets the URL of the asset.
+		/// </summary>
+		[JsonProperty("url")]
+		public string Url { get; }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Asset"/> class.
-        /// </summary>
-        [JsonConstructor]
-        internal Asset(string name, string type, int size, string url)
-        {
-            Name = name;
-            Type = type;
-            Size = size;
-            Url = url;
-        }
-    }
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Asset"/> class.
+		/// </summary>
+		[JsonConstructor]
+		internal Asset(string name, string type, int size, string url)
+		{
+			Name = name;
+			Type = type;
+			Size = size;
+			Url = url;
+		}
+	}
 }
