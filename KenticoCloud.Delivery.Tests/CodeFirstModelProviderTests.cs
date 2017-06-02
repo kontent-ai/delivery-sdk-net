e9eb11a114aeb97ea8d0e6f4b0a11dff6253c83b
Martin Knapik <knapik.martin92@gmail.com>
2017-06-02 09:12:24 +0200

    Changed JSON to object to make it more readable

--- KenticoCloud.Delivery.Tests/CodeFirstModelProviderTests.cs
+++ KenticoCloud.Delivery.Tests/CodeFirstModelProviderTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using AngleSharp.Dom.Events;
 using FakeItEasy;
 using KenticoCloud.Delivery.InlineContentItems;
@@ -18,18 +19,17 @@ namespace KenticoCloud.Delivery.Tests
             A.CallTo(() => codeFirstTypeProvider.GetType(A<string>._)).Returns(typeof(ContentItemWithSingleRTE));
             
             var processor = A.Fake<IInlineContentItemsProcessor>();
-            A.CallTo(() => processor.Process(A<string>.Ignored, A<Dictionary<string,object>>.Ignored)).Returns("a");
+            A.CallTo(() => processor.Process(A<string>.Ignored, A<Dictionary<string,object>>.Ignored));
             
             A.CallTo(() => fakeDeliverClient.InlineContentItemsProcessor).Returns(processor);
 
             var retriever = new CodeFirstModelProvider(fakeDeliverClient);
             retriever.TypeProvider = codeFirstTypeProvider;
 
-            var item = JToken.Parse(contentItemContaningRTE);
-            var modularContent = JToken.Parse(modularContentReferencingRTE);
+            var item = JToken.FromObject(rt1);
+            var modularContent = JToken.FromObject(modularContentObject);
 
             var result = retriever.GetContentItemModel<ContentItemWithSingleRTE>(item, modularContent);
-
 
             Assert.IsType<ContentItemWithSingleRTE>(result);
             A.CallTo(() => processor.Process(A<string>._, A<Dictionary<string, object>>._))
@@ -43,11 +43,60 @@ namespace KenticoCloud.Delivery.Tests
             public string RT { get; set; }
         }
 
-        private const string contentItemContaningRTE =
-            "{\r\n    \"system\": {\r\n      \"id\": \"9dc3ca3a-22e0-4414-a56d-7a504e9f1eb2\",\r\n      \"name\": \"RT1\",\r\n      \"codename\": \"rt1\",\r\n      \"type\": \"simple_richtext\",\r\n      \"sitemap_locations\": [],\r\n      \"last_modified\": \"2017-06-01T11:43:33.1968174Z\"\r\n    },\r\n    \"elements\": {\r\n      \"rt\": {\r\n        \"type\": \"rich_text\",\r\n        \"name\": \"RT\",\r\n        \"images\": {},\r\n        \"links\": {},\r\n        \"modular_content\": [\r\n          \"rt2\"\r\n        ],\r\n        \"value\": \"<p><br></p>\\n<object type=\\\"application/kenticocloud\\\" data-type=\\\"item\\\" data-codename=\\\"rt2\\\"></object>\"\r\n      }\r\n    }\r\n  }";
+        private static object rt1 = new
+        {
+            system = new
+            {
+                id = "9dc3ca3a-22e0-4414-a56d-7a504e9f1eb2",
+                name = "RT1" ,
+                codename = "rt1",
+                type = "simple_richtext",
+                sitemap_location = new string[0],
+                last_modified = new DateTime(2017,06,01, 11,43,33)
+            },
+            elements = new
+            {
+                rt = new
+                {
+                    type = "rich_text",
+                    name = "RT",
+                    modular_content = new [] { "rt2"},
+                    value = "\"<object type=\\\"application/kenticocloud\\\" data-type=\\\"item\\\" data-codename=\\\"rt2\\\"></object>\""
+                }
 
-        private const string modularContentReferencingRTE =
-            "{\r\n    \"rt2\": {\r\n      \"system\": {\r\n        \"id\": \"c7e516cb-28c9-41a4-8531-3c88a70aa54f\",\r\n        \"name\": \"RT2\",\r\n        \"codename\": \"rt2\",\r\n        \"type\": \"simple_richtext\",\r\n        \"sitemap_locations\": [],\r\n        \"last_modified\": \"2017-06-01T11:43:50.2741506Z\"\r\n      },\r\n      \"elements\": {\r\n        \"rt\": {\r\n          \"type\": \"rich_text\",\r\n          \"name\": \"RT\",\r\n          \"images\": {},\r\n          \"links\": {},\r\n          \"modular_content\": [\r\n            \"rt1\"\r\n          ],\r\n          \"value\": \"<p><br></p>\\n<object type=\\\"application/kenticocloud\\\" data-type=\\\"item\\\" data-codename=\\\"rt1\\\"></object>\"\r\n        }\r\n      }\r\n    },\r\n    \"rt1\": {\r\n      \"system\": {\r\n        \"id\": \"9dc3ca3a-22e0-4414-a56d-7a504e9f1eb2\",\r\n        \"name\": \"RT1\",\r\n        \"codename\": \"rt1\",\r\n        \"type\": \"simple_richtext\",\r\n        \"sitemap_locations\": [],\r\n        \"last_modified\": \"2017-06-01T11:43:33.1968174Z\"\r\n      },\r\n      \"elements\": {\r\n        \"rt\": {\r\n          \"type\": \"rich_text\",\r\n          \"name\": \"RT\",\r\n          \"images\": {},\r\n          \"links\": {},\r\n          \"modular_content\": [\r\n            \"rt2\"\r\n          ],\r\n          \"value\": \"<p><br></p>\\n<object type=\\\"application/kenticocloud\\\" data-type=\\\"item\\\" data-codename=\\\"rt2\\\"></object>\"\r\n        }\r\n      }\r\n    }\r\n}";
+            }
+        };
+
+        private static object rt2 = new
+        {
+            system = new
+            {
+                id = "c7e516cb-28c9-41a4-8531-3c88a70aa54f",
+                name = "RT2",
+                codename = "rt2",
+                type = "simple_richtext",
+                sitemap_location = new string[0],
+                last_modified = new DateTime(2017, 06, 01, 11, 43, 33)
+            },
+            elements = new
+            {
+                rt = new
+                {
+                    type = "rich_text",
+                    name = "RT",
+                    modular_content = new [] {"rt1"},
+                    value =
+                    "\"<object type=\\\"application/kenticocloud\\\" data-type=\\\"item\\\" data-codename=\\\"rt1\\\"></object>\""
+                }
+            }
+        };
+
+        private static object modularContentObject = new
+        {
+            rt2,
+            rt1
+        };
+
     }
 
 
