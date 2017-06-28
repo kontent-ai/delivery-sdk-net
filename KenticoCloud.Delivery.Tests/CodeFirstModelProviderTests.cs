a5f6a940a92364e6864d053619f0097ce9a7e126
Martin Knapik <knapik.martin92@gmail.com>
2017-06-28 13:47:51 +0200

    Added test for self-reference

--- KenticoCloud.Delivery.Tests/CodeFirstModelProviderTests.cs
+++ KenticoCloud.Delivery.Tests/CodeFirstModelProviderTests.cs
@@ -27,11 +27,35 @@ namespace KenticoCloud.Delivery.Tests
             A.CallTo(() => fakeDeliverClient.InlineContentItemsProcessor).Returns(processor);
 
             var item = JToken.FromObject(rt1);
-            var modularContent = JToken.FromObject(modularContentObject);
+            var modularContent = JToken.FromObject(modularContentForItemWithTwoReferencedContentItems);
 
             var result = retriever.GetContentItemModel<ContentItemWithSingleRTE>(item, modularContent);
 
             Assert.Equal("<span>FirstRT</span><span>SecondRT</span><span>FirstRT</span>", result.RT);
+            Assert.IsType<ContentItemWithSingleRTE>(result);
+        }
+
+        [Fact]
+        // In case item is referencing itself ( A refs A ) we'd like to go through second processing as if there were no inline content items,
+        // this is same as in other cases, because as soon as we start processing item which is already being processed we remove inline content items.
+        public void RetrievingContentModelWithItemInlineReferencingItselfDoesNotCycle()
+        {
+            var fakeDeliverClient = A.Fake<IDeliveryClient>();
+            var codeFirstTypeProvider = A.Fake<ICodeFirstTypeProvider>();
+            A.CallTo(() => codeFirstTypeProvider.GetType(A<string>._)).Returns(typeof(ContentItemWithSingleRTE));
+
+            var processor = new InlineContentItemsProcessor(null, null);
+            processor.RegisterTypeResolver(new RichTextInlineResolver());
+            var retriever = new CodeFirstModelProvider(fakeDeliverClient);
+            retriever.TypeProvider = codeFirstTypeProvider;
+            A.CallTo(() => fakeDeliverClient.InlineContentItemsProcessor).Returns(processor);
+
+            var item = JToken.FromObject(rt3);
+            var modularContent = JToken.FromObject(modularContentForItemReferencingItself);
+
+            var result = retriever.GetContentItemModel<ContentItemWithSingleRTE>(item, modularContent);
+
+            Assert.Equal("<span>RT</span><span>RT</span>", result.RT);
             Assert.IsType<ContentItemWithSingleRTE>(result);
         }
 
@@ -96,12 +120,40 @@ namespace KenticoCloud.Delivery.Tests
             }
         };
 
-        private static object modularContentObject = new
+        private static object modularContentForItemWithTwoReferencedContentItems = new
         {
             rt2,
             rt1
         };
 
+        private static object rt3 = new
+        {
+            system = new
+            {
+                id = "9dc3ca3a-22e0-4414-a56d-7a504e9f1eb2",
+                name = "RT3",
+                codename = "rt3",
+                type = "simple_richtext",
+                sitemap_location = new string[0],
+                last_modified = new DateTime(2017, 06, 01, 11, 43, 33)
+            },
+            elements = new
+            {
+                rt = new
+                {
+                    type = "rich_text",
+                    name = "RT",
+                    modular_content = new[] { "rt3" },
+                    value = "<span>RT</span><object type=\"application/kenticocloud\" data-type=\"item\" data-codename=\"rt3\"></object>"
+                }
+
+            }
+        };
+
+        private static object modularContentForItemReferencingItself = new
+        {
+            rt3
+        };
     }
 
 
