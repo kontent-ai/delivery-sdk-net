974ae953f1f194d39da5fb0c98ab63d8eee87876
Jan Lenoch <jan.lenoch@hotmail.cz>
2018-07-18 16:23:28 +0200

    Remove Visual Studio warnings about unresolvable references
    Probably due to some recent breaking changes in the framework, VS started showing the MSB3245 compiler warnings. It warned that some of the framework assemblies were not found in their expected filesystem locations.
    
    Although the exact cause is not known, trying to raise the `TargetFramework` to `netstandard2.0` resolved the issue. The upgrade did not incur any code changes, only the .csproj and AssemblyInfo.cs were affected. All other code changes deal with comments in https://github.com/Kentico/delivery-sdk-net/pull/115 .
    
    I've also upgraded the dependencies to their newest versions.

---
+++ KenticoCloud.Delivery.Rx.Tests/Models/ContentItemModelWithAttributes.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace KenticoCloud.Delivery.Rx.Tests
+{
+    public class ContentItemModelWithAttributes
+    {
+        [JsonProperty("text_field")]
+        public string TextFieldWithADifferentName { get; set; }
+
+        [JsonProperty("rich_text_field")]
+        public string RichTextFieldWithADifferentName { get; set; }
+
+        [JsonProperty("number_field")]
+        public decimal? NumberFieldWithADifferentName { get; set; }
+
+        [JsonProperty("multiple_choice_field_as_radio_buttons")]
+        public IEnumerable<MultipleChoiceOption> MultipleChoiceFieldAsRadioButtonsWithADifferentName { get; set; }
+
+        [JsonProperty("multiple_choice_field_as_checkboxes")]
+        public IEnumerable<MultipleChoiceOption> MultipleChoiceFieldAsCheckboxes { get; set; }
+
+        [JsonProperty("date___time_field")]
+        public DateTime? DateTimeFieldWithADifferentName { get; set; }
+
+        [JsonProperty("asset_field")]
+        public IEnumerable<Asset> AssetFieldWithADifferentName { get; set; }
+
+        [JsonProperty("modular_content_field")]
+        public IEnumerable<object> ModularContentFieldWithADifferentName { get; set; }
+
+        [JsonProperty("modular_content_field")]
+        public HashSet<object> ModularContentFieldWithACollectionTypeDefined { get; set; }
+
+        public IEnumerable<string> RandomField { get; set; }
+
+        [JsonProperty("modular_content_field")]
+        public HashSet<Homepage> ModularContentFieldWithAGenericTypeDefined { get; set; }
+
+        [JsonProperty("complete_type_taxonomy")]
+        public IEnumerable<TaxonomyTerm> CompleteTypeTaxonomyWithADifferentName { get; set; }
+        
+        public ContentItemSystemAttributes System { get; set; }
+    }
+}
