6d5837191f790d9eec034f84821e144adc8c49b9
Miroslav Kubus <MiroslavK@kentico.com>
2018-08-14 13:27:06 +0200

    KA-388 Enable automatic generation of assembly info

--- KenticoCloud.Delivery/Properties/AssemblyInfo.cs
+++ KenticoCloud.Delivery/Properties/AssemblyInfo.cs
@@ -1,9 +1,6 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyCopyright("© 2016 Kentico Software. All rights reserved.")]
 [assembly: ComVisible(false)]
 [assembly: Guid("103c7376-d90c-4207-9933-c9e55a16d902")]
-
 [assembly: InternalsVisibleTo("KenticoCloud.Delivery.Tests")]
