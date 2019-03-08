0f5b9b574cbc3c752048076c41cf397e19f86c5c
Matus Backor <MatusB@kentico.com>
2019-03-08 15:20:54 +0100

    Remove duplicate class

--- KenticoCloud.Delivery/Models/Type/Element/MultipleChoiceOption.cs
+++ KenticoCloud.Delivery/Models/Type/Element/MultipleChoiceOption.cs
@@ -3,25 +3,33 @@
 namespace KenticoCloud.Delivery
 {
     /// <summary>
-    /// Represents a selected option of a Multiple choice element.
+    /// Represents an option of a Multiple choice content element.
     /// </summary>
     public sealed class MultipleChoiceOption
     {
         /// <summary>
-        /// Gets the name of the selected option.
+        /// Gets the name of the option.
         /// </summary>
         [JsonProperty("name")]
-        public string Name { get; }
+        public string Name
+        {
+            get;
+        }
 
         /// <summary>
-        /// Gets the codename of the selected option.
+        /// Gets the codename of the option.
         /// </summary>
         [JsonProperty("codename")]
-        public string Codename { get; }
+        public string Codename
+        {
+            get;
+        }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="MultipleChoiceOption"/> class.
+        /// Initializes a new instance of the <see cref="MultipleChoiceOption"/> class with the specified JSON data.
         /// </summary>
+        /// <param name="name">Name of the option.</param>
+        /// <param name="codename">Code name of the option.</param>
         [JsonConstructor]
         internal MultipleChoiceOption(string name, string codename)
         {
