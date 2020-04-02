dce99d6118e51156f5e855a3287bb2d533a02f7d
ondrejch <ondrejch@kentico.com>
2020-04-02 15:40:01 +0200

    add test for testing complex data

---
+++ Kentico.Kontent.Delivery.Tests/Models/ContentTypes/SimpleRichText.cs
@@ -0,0 +1,11 @@
+﻿using Kentico.Kontent.Delivery.Abstractions;
+using Newtonsoft.Json;
+
+namespace Kentico.Kontent.Delivery.Tests
+{
+    public partial class SimpleRichText
+    {
+        [JsonProperty("rich_text")]
+        public IRichTextContent RichText { get; set; }
+    }
+}
