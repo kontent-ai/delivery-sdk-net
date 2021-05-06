22ff7021bc6a9307d2ca76953f901e3cbd318641
ondrejch <Simply007@users.noreply.github.com>
2021-05-06 13:07:40 +0200

    prepare a test for Link URL resolver

--- Kentico.Kontent.Delivery.Tests/ContentLinkResolverTests.cs
+++ Kentico.Kontent.Delivery.Tests/ContentLinkResolverTests.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using AngleSharp.Html.Parser;
 using Kentico.Kontent.Delivery.Abstractions;
+using Kentico.Kontent.Delivery.Builders.DeliveryClient;
 using Kentico.Kontent.Delivery.ContentItems;
 using Kentico.Kontent.Delivery.ContentItems.ContentLinks;
 using Kentico.Kontent.Delivery.RetryPolicy;
@@ -147,6 +149,77 @@ namespace Kentico.Kontent.Delivery.Tests
             var item = await client.GetItemAsync<Article>("coffee_processing_techniques");
 
             Assert.Contains(expected, item.Item.BodyCopy);
+        }
+
+        private sealed class CustomAsyncContentLinkUrlResolver : IContentLinkUrlResolver
+        {
+            public IDeliveryClient Client { get; set; }
+            public Task<string> ResolveBrokenLinkUrlAsync()
+            {
+                return Task.FromResult("broken");
+            }
+
+            public async Task<string> ResolveLinkUrlAsync(IContentLink link)
+            {
+                var response = await Client.GetItemAsync<object>(link.Codename);
+
+                switch (link.ContentTypeCodename)
+                {
+                    case ("coffee"):
+                        var caffee = response.Item as Coffee;
+                        string productCustomSlug = Regex.Replace(caffee.ProductName, @"[^A-Za-z0-9\s-]", "");
+                        // Remove all additional spaces in favour of just one.  
+                        productCustomSlug = Regex.Replace(productCustomSlug, @"\s+", " ").Trim();
+                        // Replace all spaces with the hyphen.  
+                        productCustomSlug = Regex.Replace(productCustomSlug, @"\s", "-");
+                        return $"/product/{productCustomSlug.ToLowerInvariant()}";
+
+                    case ("article"):
+                        var article = response.Item as Article;
+                        string articleCustomSlug = Regex.Replace(article.Title, @"[^A-Za-z0-9\s-]", "");
+                        // Remove all additional spaces in favour of just one.  
+                        articleCustomSlug = Regex.Replace(articleCustomSlug, @"\s+", " ").Trim();
+                        // Replace all spaces with the hyphen.  
+                        articleCustomSlug = Regex.Replace(articleCustomSlug, @"\s", "-");
+
+                        return $"/blog/{articleCustomSlug.ToLowerInvariant()}";
+                    default:
+                        return $"/404"; ;
+                }
+            }
+        }
+
+        [Fact]
+        public async void CallDeliveryRequestInCustomContentLinkResolver()
+        {
+            var mockHttp = new MockHttpMessageHandler();
+            string guid = Guid.NewGuid().ToString();
+            string coffeUrl = $"https://deliver.kontent.ai/{guid}/items/coffee_processing_techniques";
+            mockHttp.When(coffeUrl).
+               Respond("application/json", await File.ReadAllTextAsync(Path.Combine(Environment.CurrentDirectory, $"Fixtures{Path.DirectorySeparatorChar}ContentLinkResolver{Path.DirectorySeparatorChar}coffee_processing_techniques.json")));
+            string brazilNaturalCoffeeUrl = $"https://deliver.kontent.ai/{guid}/items/brazil_natural_barra_grande";
+            mockHttp.When(brazilNaturalCoffeeUrl).
+               Respond("application/json", await File.ReadAllTextAsync(Path.Combine(Environment.CurrentDirectory, $"Fixtures{Path.DirectorySeparatorChar}DeliveryClient{Path.DirectorySeparatorChar}brazil_natural_barra_grande.json")));
+            string kenyaCoffee = $"https://deliver.kontent.ai/{guid}/items/kenya_gakuyuni_aa";
+            mockHttp.When(kenyaCoffee).
+               Respond("application/json", await File.ReadAllTextAsync(Path.Combine(Environment.CurrentDirectory, $"Fixtures{Path.DirectorySeparatorChar}DeliveryClient{Path.DirectorySeparatorChar}kenya_gakuyuni_aa.json")));
+
+            var resolver = new CustomAsyncContentLinkUrlResolver();
+            var client = DeliveryClientBuilder
+                .WithProjectId(guid)
+                .WithTypeProvider(new CustomTypeProvider())
+                .WithContentLinkUrlResolver(resolver)
+                .WithDeliveryHttpClient(new DeliveryHttpClient(mockHttp.ToHttpClient()))
+                .Build();
+            // hardcode the reference to the client
+            resolver.Client = client;
+
+            string expectedCoffee1 = "<a data-item-id=\"80c7074b-3da1-4e1d-882b-c5716ebb4d25\" href=\"/product/kenya-gakuyuni-aa\">Kenya Gakuyuni AA</a>";
+            string expectedCoffee2 = "<a data-item-id=\"0c9a11bb-6fc3-409c-b3cb-f0b797e15489\" href=\"/product/brazil-natural-barra-grande\">Brazil Natural Barra Grande</a>";
+            var item = await client.GetItemAsync<Article>("coffee_processing_techniques");
+
+            Assert.Contains(expectedCoffee1, item.Item.BodyCopy);
+            Assert.Contains(expectedCoffee2, item.Item.BodyCopy);
         }
 
         private async Task<string> ResolveContentLinks(string text)
