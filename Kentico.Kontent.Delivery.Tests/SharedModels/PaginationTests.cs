ba917195f573b9371c884a1675a913b30de1fad3
John Hyde <johnhydemtm365>
2021-10-22 09:02:22 +0100

    Fixed the class! Issue:285

--- Kentico.Kontent.Delivery.Tests/SharedModels/PaginationTests.cs
+++ Kentico.Kontent.Delivery.Tests/SharedModels/PaginationTests.cs
@@ -9,30 +9,36 @@ namespace Kentico.Kontent.Delivery.Tests.SharedModels
     public class PaginationTests
     {
         [Fact]
-        public void SerializePagination_AndThen_DeserializeToCheck_ValuesMatch_UsingBsonSerialization()
+        public void SerializePagination_AndThen_DeserializeToCheck_ValuesMatch_UsingStandardSerialization()
         {
-        private void ExecuteTestsScenario(Func<Pagination,Pagination> serializeThenDeserialize)
-        {
-            var pagination = new Pagination(2, 500, 10, 65, "https://nextpageUrl");
-            var json = JsonConvert.SerializeObject(pagination);
-            var deserialisedPagination = JsonConvert.DeserializeObject<Pagination>(json);
-            Assert.Equal(pagination.Skip, deserialisedPagination.Skip);
-            Assert.Equal(pagination.Limit, deserialisedPagination.Limit);
-            Assert.Equal(pagination.Count, deserialisedPagination.Count);
-            Assert.Equal(pagination.TotalCount, deserialisedPagination.TotalCount);
-            Assert.Equal(pagination.NextPageUrl, deserialisedPagination.NextPageUrl);
+            ExecuteTestsScenario(SerializeThenDeserializeUsingJson);
         }
         [Fact]
-        public void SerialisePagination_AndThen_DeserialiseToCheck_ValuesMatch_UsingBsonSerialisation()
+        public void SerializePagination_AndThen_DeserializeToCheck_ValuesMatch_UsingBsonSerialization()
+        {
+            ExecuteTestsScenario(SerializeThenDeserializeUsingBson);
+        }
+        private void ExecuteTestsScenario(Func<Pagination, Pagination> serializeThenDeserialize)
+        {
+            var pagination = new Pagination(2, 500, 10, 65, "https://nextpageUrl");
+            var deserializedPagination = serializeThenDeserialize(pagination);
+            Assert.Equal(pagination.Skip, deserializedPagination.Skip);
+            Assert.Equal(pagination.Limit, deserializedPagination.Limit);
+            Assert.Equal(pagination.Count, deserializedPagination.Count);
+            Assert.Equal(pagination.TotalCount, deserializedPagination.TotalCount);
+            Assert.Equal(pagination.NextPageUrl, deserializedPagination.NextPageUrl);
+        }
+
+        private Pagination SerializeThenDeserializeUsingJson(Pagination pagination)
+        {
+            var json = JsonConvert.SerializeObject(pagination);
+            return JsonConvert.DeserializeObject<Pagination>(json);
+        }
+
+        private Pagination SerializeThenDeserializeUsingBson(Pagination pagination)
         {
             var data = pagination.ToBson();
-            //convert back
-            var deserialisedPagination = data.FromBson<Pagination>();
-            Assert.Equal(pagination.Skip, deserialisedPagination.Skip);
-            Assert.Equal(pagination.Limit, deserialisedPagination.Limit);
-            Assert.Equal(pagination.Count, deserialisedPagination.Count);
-            Assert.Equal(pagination.TotalCount, deserialisedPagination.TotalCount);
-            Assert.Equal(pagination.NextPageUrl, deserialisedPagination.NextPageUrl);
+            return data.FromBson<Pagination>();
         }
     }
 }
