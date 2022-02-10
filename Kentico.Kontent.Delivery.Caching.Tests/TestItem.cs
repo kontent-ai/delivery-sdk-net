1eb6016a0b9571b2e77f6d61e292136855c5419f
Tomas Winkler <TomasW@kentico.com>
2022-02-10 10:37:51 +0100

    279 - enable to write a second value converter for the same field and showcase on linked item codenames

--- Kentico.Kontent.Delivery.Caching.Tests/TestItem.cs
+++ Kentico.Kontent.Delivery.Caching.Tests/TestItem.cs
@@ -1,9 +1,12 @@
-﻿using Kentico.Kontent.Delivery.Abstractions;
+﻿using System;
+using Kentico.Kontent.Delivery.Abstractions;
+using Kentico.Kontent.Delivery.ContentItems;
 
 namespace Kentico.Kontent.Delivery.Caching.Tests
 {
     public class TestItem
     {
+        [PropertyName("property_name_does_not_affect_caching")]
         public string Title { get; set; }
         public IContentItemSystemAttributes System { get; set; }
     }
---
+++ Kentico.Kontent.Delivery/ContentItems/PropertyNameAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Kentico.Kontent.Delivery.ContentItems
+{
+    /// <summary>
+    /// Instructs the <see cref="PropertyMapper"/> to always map to the property with the specified name.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class PropertyNameAttribute : Attribute
+    {
+        /// <summary>
+        /// Gets or sets the name of the property.
+        /// </summary>
+        /// <value>The name of the property.</value>
+        public string PropertyName { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PropertyNameAttribute"/> class.
+        /// </summary>
+        public PropertyNameAttribute(string propertyName)
+        {
+            PropertyName = propertyName;
+        }
+    }
+}
