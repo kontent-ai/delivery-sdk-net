86af9382f38eedf22b4631c31cf6a0ab8f9d1c52
Daniel Pokorny <danielp@kentico.com>
2022-07-19 18:05:54 +0200

    migration: rename folders

---
+++ Kontent.Ai.Delivery.Tests/Models/ContentTypes/SimpleRichText.cs
@@ -0,0 +1,14 @@
+﻿using Kentico.Kontent.Delivery.Abstractions;
+using Newtonsoft.Json;
+
+namespace Kentico.Kontent.Delivery.Tests.Models.ContentTypes
+{
+    public class SimpleRichText
+    {
+        [JsonProperty("rich_text")]
+        public IRichTextContent RichText { get; set; }
+
+        [JsonProperty("rich_text")]
+        public string RichTextString { get; set; }
+    }
+}
