f3125d3d47b784d84a5e5de2c75a70fa39037886
Daniel Pokorny <danielp@kentico.com>
2022-07-22 15:31:20 +0200

    migration: fix faulty names in assemblyinfo

--- Kontent.Ai.Delivery.Tests/Properties/AssemblyInfo.cs
+++ Kontent.Ai.Delivery.Tests/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Kontent.Ai.Delivery.Tests")]
 [assembly: AssemblyDescription("Kontent.ai Delivery SDK tests")]
-[assembly: AssemblyCompany("Kontent.ai s.r.o.")]
+[assembly: AssemblyCompany("Kontent s.r.o.")]
 [assembly: AssemblyProduct("Kontent.ai")]
-[assembly: AssemblyCopyright("© 2022 Kontent.ai s.r.o. All rights reserved.")]
+[assembly: AssemblyCopyright("© 2022 Kontent s.r.o. All rights reserved.")]
 [assembly: ComVisible(false)]
 [assembly: Guid("eb5970de-f2a5-492a-ab3c-87a9212584e1")]
 [assembly: AssemblyVersion("1.0.0.0")]
