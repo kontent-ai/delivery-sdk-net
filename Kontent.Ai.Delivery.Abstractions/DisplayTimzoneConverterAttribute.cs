a4df9ffc05ec14b6adb525a95a4757609edb30e3
Milan Jankovic <MilanJ@kontent.ai>
2022-11-16 14:27:06 +0100

    Add DisplayTimezone converter atttribute

---
+++ Kontent.Ai.Delivery.Abstractions/DisplayTimzoneConverterAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Kontent.Ai.Delivery.Abstractions;
+
+namespace Kontent.Ai.Delivery.Abstractions
+{
+    /// <summary>
+    /// Specifies property of a model to map DisplayTimezone value into
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class DisplayTimzoneConverterAttribute : Attribute, IPropertyValueConverter<DateTime>
+    {
+        Task<object> IPropertyValueConverter<DateTime>.GetPropertyValueAsync<TElement>(PropertyInfo property, TElement element, ResolvingContext context)
+        {
+            if (element is not IDateTimeElementValue dateTimeElement)
+            {
+                return null;
+            }
+
+            return Task.FromResult<object>(dateTimeElement.DisplayTimezone);
+        }
+    }
+}
