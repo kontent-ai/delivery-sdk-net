a31cc0c775edb464ff470acf04bf028ad55a0941
Dzmitry Kuliashou <dzmitryk@kontent.ai>
2023-02-16 18:38:20 +0100

    358 Add WithLoggerFactory extension method

--- Kontent.Ai.Delivery.Caching/Extensions/ServiceCollectionExtensions.cs
+++ Kontent.Ai.Delivery.Caching/Extensions/ServiceCollectionExtensions.cs
@@ -4,8 +4,6 @@ using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Abstractions;
 
 namespace Kontent.Ai.Delivery.Caching.Extensions
 {
@@ -19,27 +17,21 @@ namespace Kontent.Ai.Delivery.Caching.Extensions
         /// </summary>
         /// <param name="services">A <see cref="IServiceCollection"/> instance for registering and resolving dependencies.</param>
         /// <param name="options">A <see cref="DeliveryCacheOptions"/> instance.</param>
-        /// <param name="loggerFactory">The factory used to create loggers.</param>
         /// <returns>The <paramref name="services"/> instance with cache services registered in it</returns>
         public static IServiceCollection AddDeliveryClientCache(this IServiceCollection services,
-            DeliveryCacheOptions options,
-            ILoggerFactory loggerFactory = null)
+            DeliveryCacheOptions options)
         {
             if (options == null)
             {
                 throw new ArgumentNullException(nameof(options), "The Delivery cache  options object is not specified.");
             }
 
-            var loggerFactoryToRegister = loggerFactory ?? NullLoggerFactory.Instance;
-
             return services
                  .Configure<DeliveryCacheOptions>((o) => o.Configure(options))
-                 .RegisterDependencies(options.CacheType, loggerFactoryToRegister)
+                 .RegisterDependencies(options.CacheType)
                  .Decorate<IDeliveryClient, DeliveryClientCache>();
         }
-        private static IServiceCollection RegisterDependencies(this IServiceCollection services,
-            CacheTypeEnum cacheType,
-            ILoggerFactory loggerFactory)
+        private static IServiceCollection RegisterDependencies(this IServiceCollection services, CacheTypeEnum cacheType)
         {
             switch (cacheType)
             {
@@ -51,7 +43,6 @@ namespace Kontent.Ai.Delivery.Caching.Extensions
                 case CacheTypeEnum.Distributed:
                     services.TryAddSingleton<IDeliveryCacheManager, DistributedCacheManager>();
                     services.TryAddSingleton<IDistributedCache, MemoryDistributedCache>();
-                    services.TryAddSingleton(loggerFactory);
                     break;
             }
 
