509cad758720601bca8c5768d796f1ea04d6eebc
Martin Ševčík <sevcik.martin4@gmail.com>
2023-03-03 12:31:39 +0100

    368 introduce IContentItem

---
+++ Kontent.Ai.Delivery.Abstractions/ContentItems/IContentItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Kontent.Ai.Delivery.Abstractions;
+
+/// <summary>
+/// Represents content item.
+/// </summary>
+public interface IContentItem
+{
+    /// <summary>
+    /// Represents system attributes of a content item.
+    /// </summary>
+    public IContentItemSystemAttributes System { get; set; }
+}
