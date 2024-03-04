9327b2c924d187e5f422072f6b7ce64f6de6ee46
zdenekj <zdenekj@kontent.ai>
2024-03-04 11:55:58 +0100

    Rename ExcludeParameter to ExcludeElementsParameter

--- Kontent.Ai.Urls/Delivery/QueryParameters/ExcludeElementsParameter.cs
+++ Kontent.Ai.Urls/Delivery/QueryParameters/ExcludeElementsParameter.cs
@@ -8,7 +8,7 @@ namespace Kontent.Ai.Urls.Delivery.QueryParameters
     /// <summary>
     /// Specifies the content elements to exclude from response.
     /// </summary>
-    public sealed class ExcludeParameter : IQueryParameter
+    public sealed class ExcludeElementsParameter : IQueryParameter
     {
         /// <summary>
         /// Gets the list of codenames of content elements that should be excluded from response.
@@ -16,10 +16,10 @@ namespace Kontent.Ai.Urls.Delivery.QueryParameters
         public IReadOnlyList<string> ElementCodenames { get; }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="ExcludeParameter"/> class using the specified content elements codenames.
+        /// Initializes a new instance of the <see cref="ExcludeElementsParameter"/> class using the specified content elements codenames.
         /// </summary>
         /// <param name="elementCodenames">An array that contains zero or more codenames of the content elements that should be excluded from response.</param>
-        public ExcludeParameter(params string[] elementCodenames)
+        public ExcludeElementsParameter(params string[] elementCodenames)
         {
             ElementCodenames = elementCodenames.ToList().AsReadOnly();
         }
@@ -29,7 +29,7 @@ namespace Kontent.Ai.Urls.Delivery.QueryParameters
         /// </summary>
         public string GetQueryStringParameter()
         {
-            return $"exclude={string.Join(Uri.EscapeDataString(","), ElementCodenames.Select(Uri.EscapeDataString))}";
+            return $"excludeElements={string.Join(Uri.EscapeDataString(","), ElementCodenames.Select(Uri.EscapeDataString))}";
         }
     }
 }
