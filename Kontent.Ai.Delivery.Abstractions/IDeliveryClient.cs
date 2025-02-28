7ead477aaec17085a2741e8ef59ab9b7fc767c00
tomasw_kontent <tomasw@kontent.ai>
2025-02-28 14:47:46 +0100

    fix used in methods documentation

--- Kontent.Ai.Delivery.Abstractions/IDeliveryClient.cs
+++ Kontent.Ai.Delivery.Abstractions/IDeliveryClient.cs
@@ -94,7 +94,7 @@ namespace Kontent.Ai.Delivery.Abstractions
         /// Returns a feed that is used to traverse through strongly typed parent content items matching the optional filtering parameters.
         /// </summary>
         /// <param name="codename">The codename of a content item.</param>
-        /// <param name="parameters">A collection of query parameters, for example, for filtering or ordering.</param>
+        /// <param name="parameters">A collection of query parameters for filtering.</param>
         /// <returns>The <see cref="IDeliveryItemsFeed{IUsedInItem}"/> instance that can be used to enumerate through content item parents for the specified item codename. If no query parameters are specified, default language parents are enumerated.</returns>
         public IDeliveryItemsFeed<IUsedInItem> GetItemUsedIn(string codename, IEnumerable<IQueryParameter> parameters = null);
 
@@ -102,7 +102,7 @@ namespace Kontent.Ai.Delivery.Abstractions
         /// Returns a feed that is used to traverse through strongly typed parent content items matching the optional filtering parameters.
         /// </summary>
         /// <param name="codename">The codename of an asset.</param>
-        /// <param name="parameters">A collection of query parameters, for example, for filtering or ordering.</param>
+        /// <param name="parameters">A collection of query parameters for filtering.</param>
         /// <returns>The <see cref="IDeliveryItemsFeed{IUsedInItem}"/> instance that can be used to enumerate through asset parents for the specified asset codename. If no query parameters are specified, default language parents are enumerated.</returns>
         public IDeliveryItemsFeed<IUsedInItem> GetAssetUsedIn(string codename, IEnumerable<IQueryParameter> parameters = null);
     }
