4d5caacc20cfc12e061052cd31f127d8d0333b3e
Martin Ševčík <martins2@kontent.ai>
2025-05-15 14:45:15 +0200

    404 use file scoped namespace

--- Kontent.Ai.Delivery/Sync/SyncItemData.cs
+++ Kontent.Ai.Delivery/Sync/SyncItemData.cs
@@ -2,24 +2,23 @@ using System.Collections.Generic;
 using Kontent.Ai.Delivery.Abstractions;
 using Newtonsoft.Json;
 
-namespace Kontent.Ai.Delivery.Sync
+namespace Kontent.Ai.Delivery.Sync;
+
+internal sealed class SyncItemData : ISyncItemData
 {
-    internal sealed class SyncItemData : ISyncItemData
+    /// <inheritdoc/>
+    [JsonProperty("system")]
+    public IContentItemSystemAttributes System { get; internal set; }
+
+    /// <inheritdoc/>
+    [JsonProperty("elements")]
+    public Dictionary<string, object> Elements { get; internal set; }
+
+    /// <summary>
+    /// Constructor used for deserialization. Contains no logic.
+    /// </summary>
+    [JsonConstructor]
+    public SyncItemData()
     {
-        /// <inheritdoc/>
-        [JsonProperty("system")]
-        public IContentItemSystemAttributes System { get; internal set; }
-
-        /// <inheritdoc/>
-        [JsonProperty("elements")]
-        public Dictionary<string, object> Elements { get; internal set; }
-
-        /// <summary>
-        /// Constructor used for deserialization. Contains no logic.
-        /// </summary>
-        [JsonConstructor]
-        public SyncItemData()
-        {
-        }
     }
 }
