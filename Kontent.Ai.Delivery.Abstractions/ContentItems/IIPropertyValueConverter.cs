a082dedec4dc55ef586c1dee8b5406246c2bb4b3
Daniel Pokorny <ext_daniel.pokorny@kontent.ai>
2025-09-08 13:41:13 +0200

    Add TResult parameter to IIPropertyValueConverter

--- Kontent.Ai.Delivery.Abstractions/ContentItems/IIPropertyValueConverter.cs
+++ Kontent.Ai.Delivery.Abstractions/ContentItems/IIPropertyValueConverter.cs
@@ -6,7 +6,7 @@ namespace Kontent.Ai.Delivery.Abstractions
     /// <summary>
     /// Provides value conversion for the given property
     /// </summary>
-    public interface IPropertyValueConverter<in T> : IPropertyValueConverter
+    public interface IPropertyValueConverter<in T, TResult> : IPropertyValueConverter
     {
         /// <summary>
         /// Gets the property value from property data
@@ -14,7 +14,7 @@ namespace Kontent.Ai.Delivery.Abstractions
         /// <param name="property">Property info</param>
         /// <param name="element">Source element data</param>
         /// <param name="context">Context of the current resolving process</param>
-        Task<object> GetPropertyValueAsync<TElement>(PropertyInfo property, TElement element, ResolvingContext context) where TElement : IContentElementValue<T>;
+        Task<TResult?> GetPropertyValueAsync<TElement>(PropertyInfo property, TElement element, ResolvingContext context) where TElement : IContentElementValue<T>;
     }
 
     /// <summary>
