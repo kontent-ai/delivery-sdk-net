08786d19d5a76b03ee53342f90e52992f99e063d
Daniel Pokorny <ext_daniel.pokorny@kontent.ai>
2025-10-09 12:07:56 +0200

    cleanup unused code

--- Kontent.Ai.Delivery/ContentItems/InlineContentItems/ReplaceWithWarningAboutUnknownItemResolver.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Kontent.Ai.Delivery.ContentItems.InlineContentItems;
-
-internal class ReplaceWithWarningAboutUnknownItemResolver : IInlineContentItemsResolver<UnknownContentItem>
-{
-    public string Resolve(UnknownContentItem item)
-        => $"Content type '{item.Type}' has no corresponding model.";
-}
--- Kontent.Ai.Delivery/ContentItems/InlineContentItems/UnretrievedContentItem.cs
+++
@@ -1,8 +0,0 @@
-namespace Kontent.Ai.Delivery.ContentItems;
-
-/// <summary>
-/// Type used to identify inline content items which were not retrieved from Deliver API.
-/// </summary>
-public class UnretrievedContentItem
-{
-}
