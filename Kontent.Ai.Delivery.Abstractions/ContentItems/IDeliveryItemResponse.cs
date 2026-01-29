4be533e77a24ee051e025a372ed07bb6bdd52d78
Daniel Pokorny <52500882+pokornyd@users.noreply.github.com>
2026-01-29 11:45:48 +0100

    update release action, fix xml doc issues

--- Kontent.Ai.Delivery.Abstractions/ContentItems/IDeliveryItemResponse.cs
+++ Kontent.Ai.Delivery.Abstractions/ContentItems/IDeliveryItemResponse.cs
@@ -2,7 +2,6 @@ namespace Kontent.Ai.Delivery.Abstractions;
 
 /// <summary>
 /// Represents a response from Kontent.ai Delivery API that contains a content item.
-
 /// Internal: Used only for JSON deserialization, then mapped to domain models.
 /// </summary>
 /// <typeparam name="TModel">The type of a content item in the response.</typeparam>
